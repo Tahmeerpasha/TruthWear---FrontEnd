693151ae348a08f9c0cc818c340d87ab2c9014dc
Tahmeerpasha <tahmeerpashacjc@gmail.com>
2023-12-14 13:16:52 +0530

    Conditionally render cart summary on when there is an item in the cart

--- components/User/CartSummary.jsx
+++ components/User/CartSummary.jsx
@@ -6,26 +6,30 @@ const CartSummary = () => {
     const cart = useSelector((state) => state.cart)
     return (
         <div className='p-5'>
-            <span className='p-2 underline text-black text-3xl font-bold'>Order Summary</span>
-            <div className='flex flex-col p-5'>
-                <div className='grid grid-cols-2 justify-end'>
-                    <span className=''>Total Items Price:</span>
-                    <span className=''> Rs.{cart.itemPrice}/-</span>
-                </div>
-                <div className='grid grid-cols-2'>
-                    <span>Tax Cost: </span>
-                    <span>Rs.{cart.taxPrice}/-</span>
-                </div>
-                <div className='grid grid-cols-2'>
-                    <span>Shipping Price:</span>
-                    <span> Rs.{cart.shippingPrice}/-</span>
-                </div>
-                <div className='grid grid-cols-2'>
-                    <span>Total Price:</span>
-                    <span> Rs.{cart.totalPrice}/-</span>
-                </div>
-            </div>
-            <Button className='w-full'>Checkout</Button>
+            {cart.itemPrice > 0 ?
+                <>
+                    <span className='p-2 underline text-black text-3xl font-bold'>Order Summary</span>
+                    <div className='flex flex-col p-5'>
+                        <div className='grid grid-cols-2 justify-end'>
+                            <span className=''>Total Items Price:</span>
+                            <span className=''> Rs.{cart.itemPrice}/-</span>
+                        </div>
+                        <div className='grid grid-cols-2'>
+                            <span>Tax Cost: </span>
+                            <span>Rs.{cart.taxPrice}/-</span>
+                        </div>
+                        <div className='grid grid-cols-2'>
+                            <span>Shipping Price:</span>
+                            <span> Rs.{cart.shippingPrice}/-</span>
+                        </div>
+                        <div className='grid grid-cols-2'>
+                            <span>Total Price:</span>
+                            <span> Rs.{cart.totalPrice}/-</span>
+                        </div>
+                    </div>
+                    <Button className='w-full'>Checkout</Button>
+                </>
+                : ''}
         </div>
     )
 }
